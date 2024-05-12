b62a053d3481245a5138b3398bdd6e2673018426
SummerinN <113506758+SummerinN@users.noreply.github.com>
2024-05-12 12:57:06 +0100

    Update nav.js

--- nav.js
+++ nav.js
@@ -8,3 +8,29 @@ function showContent(sectionId) {
       }
   }
 }
+
+document.querySelectorAll('.Projects').forEach(function(project) {
+    project.addEventListener('click', function() {
+        var details = this.querySelector('.details');
+        if (details.style.display === 'none') {
+            details.style.display = 'block';
+            details.classList.add('open');
+        } else {
+            details.classList.remove('open');
+            setTimeout(function() {
+                details.style.display = 'none';
+            }, 500); // 等待动画完成
+        }
+    });
+});
+
+document.querySelectorAll('.close-btn').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        e.stopPropagation(); // 阻止事件冒泡
+        var details = this.parentElement;
+        details.classList.remove('open');
+        setTimeout(function() {
+            details.style.display = 'none';
+        }, 500); // 等待动画完成
+    });
+});
