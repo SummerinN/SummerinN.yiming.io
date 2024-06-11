b7f32870bfd84f423ea1aff65ebb841f4bc97cb1
SummerinN <113506758+SummerinN@users.noreply.github.com>
2024-06-11 18:17:42 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-function showContent(sectionId) {
-  var sections = document.getElementsByClassName('content-section');
-  for (var i = 0; i < sections.length; i++) {
-      if (sections[i].id === sectionId) {
-          sections[i].classList.add('active');
-      } else {
-          sections[i].classList.remove('active');
-      }
-  }
-}
+
